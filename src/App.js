a564169e5bd31d267b223e0610bb619d8bb0a05d
Yasmany Hernandez <63424718+yasmoicc@users.noreply.github.com>
2022-07-23 10:28:37 -0500

    Creado el CRUD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 
-import axios from 'axios'
+
 import React, { useEffect, useState } from 'react';
 import Viewperson from './componentes/ViewPerson';
+import crud from './services/crud';
 
 function App() {
   const [people, setPeople] = useState([])
   const [newperson, setNewpeson] = useState('')
   const [newtelefono, setNewtelefono] = useState('')
-
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/people')
-      .then(response => {
-        console.log(response.data)
-        setPeople(response.data)
-      })
-  }
   
-  useEffect(hook, [])
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
+
+  useEffect(()=>{
+    crud
+      .getAll()
+      .then(inicialP => {
+        setPeople(inicialP)
+      })
+  }, []) 
+
 
   const handlePersonChange = (event) => {
     console.log(event.target.value)
@@ -36,12 +36,37 @@ function App() {
       number: newtelefono,     
       id: people.length + 1,
     }
-  
-    setPeople(people.concat(personObject))
-    setNewpeson('')
-    setNewtelefono('')
+    
+    crud
+    .create(personObject)
+    .then(returnperson => {
+      setPeople(people.concat(returnperson))
+      setNewpeson('')
+      setNewtelefono('')
+    })
+    
   }
 
+  const deletetoggle = id => {    
+    const personE = people.find(n => n.id === id)        
+    crud
+      .deleteperson(id)
+      .then(returnedNote => {        
+        setPeople(returnedNote)
+      }) 
+      .catch(error => {
+        setErrorMessage(
+          `Note '${personE.name}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)     
+        setPeople(people.filter(n => n.id !== id))
+      }) 
+    }
+
+ 
+  
   return (
     <div>
        <form onSubmit={addPerson}>
@@ -56,7 +81,20 @@ function App() {
         <button type="submit">save</button>
       </form>  
 
-      <Viewperson lista = {people}/> 
+      <ul>
+        {
+          
+          people.map(p => 
+            <Viewperson 
+              key={p.id}
+              name={p.name}
+              telefono={p.number}
+              deletehandle ={()=>deletetoggle(p.id)}
+            /> 
+          )
+          }
+      </ul>
+     
     </div>
   );
 }
--- src/componentes/ViewPerson.js
+++ src/componentes/ViewPerson.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Viewperson = ({lista}) =>{
+const Viewperson = ({key, name, telefono, deletehandle}) =>{
+    console.log(key)
+    if(key === undefined)
+        return "No hay elementos en la lista"
     return (
         <div>
-            <ul>
-                {lista.map(p => 
-                    <li key={p.id}>
-                        Name={p.name} -- Telefono={p.number}
-                    </li> 
-                )}
-            </ul>
+           <li key={key}>
+                Name={name} -- Telefono={telefono}
+                <button onClick={deletehandle}> deleteperson</button>
+            </li> 
         </div>
     );
 }
